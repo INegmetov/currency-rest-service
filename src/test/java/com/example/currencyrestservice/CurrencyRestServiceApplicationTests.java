9b0f4f096750654092079bcb3d233ad2020be3c3
inegmetov <negmetov.i@gmail.com>
2021-09-05 11:44:33 +0400

    Initial commit

---
+++ src/test/java/com/example/currencyrestservice/CurrencyRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
